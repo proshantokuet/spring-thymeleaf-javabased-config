c2cd39ec9c9db231010cce1a22b7271975d8a41e
Proshanto <proshanto@mpower-social.com>
2017-09-13 18:59:28 +0600

    add bootstrap

--- spring-thyme-leaf-web/src/main/java/com/proshanto/spring/config/MvcConfig.java
+++ spring-thyme-leaf-web/src/main/java/com/proshanto/spring/config/MvcConfig.java
@@ -27,7 +27,7 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
 	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
 	
 	@Override
